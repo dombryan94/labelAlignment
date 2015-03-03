22e30b655e26f486f13755fff29bdffdf90177ea
Dom Bryan <dombguitar@gmail.com>
2015-03-03 09:39:46 +0000

    Simple Label alignment
    Simple Label alignment, used to help people learning labels

--- labelAlignment/ViewController.swift
+++ labelAlignment/ViewController.swift
@@ -10,14 +10,28 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    
+    
+    @IBOutlet weak var exampleTextView: UITextView!
+    @IBOutlet weak var exampleLabel: UILabel!
+    var exampleText = "Lorem ipsum dolor sit er elit lamet, consectetaur cillium adipisicing pecu, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Nam liber te conscient to factor tum poen legum odioque civiuda. "
+    
+    
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        
+        //Label
+        exampleLabel.textAlignment = NSTextAlignment.Justified
+        exampleLabel.text = exampleText
+        
+        //Text View
+        exampleTextView.textAlignment = NSTextAlignment.Justified
+        exampleTextView.text = exampleText
     }
 
 
